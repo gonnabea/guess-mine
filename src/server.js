bb7130e9ecbee7271b19342d5662c049a17702cc
Choi Jiwon <winning1993@naver.com>
2020-04-25 08:58:58 +0900

    #2.0 Hello World part One

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4001;
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => {
     res.render("home");
@@ -14,4 +16,14 @@ app.get("/", (req, res) => {
 
 const handleListening = () => console.log(`<OK> Server running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening)
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", socket => {
+    sockets.push(socket.id)
+});
+
+setInterval(() => console.log(sockets), 1000);
